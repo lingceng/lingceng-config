1a7ce796637642849c7150da6c01b2c42ddd70e4
zhongkx <zhongkx@reocar.com>
2014-09-30 16:54:30 +0800

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+BASEDIR=$(dirname $0)
+VIM_PLUGIN_PATH=~/.vim/plugin
+HOME_PATH=~
+
+echo "Create ${VIM_PLUGIN_PATH} links..."
+cd ${VIM_PLUGIN_PATH}
+ln -fs ${BASEDIR}/wipeout.vim
+ln -fs ${BASEDIR}/grep-operator.vim
+echo "Dne!"
+
+echo "Create home config links..."
+cd ${HOME_PATH}
+ln -fs ${BASEDIR}/_bash_aliases .bash_aliases
+ln -fs ${BASEDIR}/_gitconfig .gitconfig
+ln -fs ${BASEDIR}/_gitignore_global .gitignore_global
+ln -fs ${BASEDIR}/_vimrc .vimrc
+echo "Done!"
+
+
